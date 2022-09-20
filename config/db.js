6d5e740ff635fa60655af356245d372eb4e9d9b9
yotamc19 <yotamc19@gmail.com>
2022-09-20 15:05:08 +0300

    initial commit

---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        const MONGO_URI = process.env.MONGO_URI;
+        if (!MONGO_URI) throw new Error("MONGO_URI not found.");
+        const conn = await mongoose.connect(MONGO_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error:${error.message}`);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
